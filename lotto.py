837ea7f590b26fea0b3da457bdfa8695a09755b6
Sherwyne R. Costiniano <sherwyne.costiniano@gmail.com>
2023-05-18 19:43:58 +0800

    Add files via upload

--- lotto.py
+++ lotto.py
@@ -1,7 +1,8 @@
 import json
 from flask import Flask, jsonify
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "localhost:3000"}})
 @app.route('/')
 def index():
     return 'Ltech Lotto API'
